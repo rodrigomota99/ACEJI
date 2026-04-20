39a3a92365b788e01c148501a7b49e05c5212e07
rodrigomota99 <digodamota99@gmail.com>
2026-04-20 17:06:21 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Este arquivo é o Service Worker.
+// Ele fica vazio por enquanto, pois serve apenas para o navegador do celular reconhecer o seu site como um App instalável.
+self.addEventListener('fetch', function(event) {
+  // Passagem direta sem cache local complexo
+});
